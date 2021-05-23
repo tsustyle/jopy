8394dea7f5bfd7409bd7e9cb0b374c8cfca8a24e
tsustyle <tsustylez@gmail.com>
2021-05-23 01:48:56 -0400

    fixed numbering bug

--- jopy.py
+++ jopy.py
@@ -5,7 +5,7 @@ try:
     path, platform, name = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Cleaning up the export because Joplin likes to export all of the parent notebooks
-    md_file = glob.glob(path + "/**/*.md", recursive=True)
+    md_file = glob.glob(path + "**/*.md", recursive=True)
     shutil.move(md_file[0], path + 'file.md')
 
     files = os.listdir(path)
@@ -27,16 +27,18 @@ try:
 
     # writes the new file with correct links
     def write_new_file():
-        file_input = open(markdown_path, "rt")
-        file_output = open(path + f'{todays_date}-{name}.md', 'wt')
+        file_input = open(markdown_path, "r")
+        file_output = open(path + f'{todays_date}-{name}.md', 'w')
         link_list = rename_png_and_make_link_list()
 
-        for i in link_list:
-            for line in file_input:
-                if line.strip().endswith('.png)'):
-                    file_output.write('\n' + i + '\n')
-                else:
-                    file_output.write(line)
+        #for i in link_list:
+        x = 0
+        for line in file_input:
+            if line.strip().endswith('.png)'):
+                file_output.write('\n' + link_list[x] + '\n')
+                x += 1
+            else:
+                file_output.write(line)
 
         file_input.close()
         file_output.close()
@@ -61,4 +63,4 @@ try:
     write_new_file()
     print('[+] Done! [+]\n')
 except:
-    print('\n[!] Usage: python3 jopy.py /path/to/export/folder category name [!]\n')
+    print('\n[!] Usage: python3 jopy.py /path/to/export/folder/ category name [!]\n')
